6329cad1dc2750743552f788d080b7a9da6a4851
abarinoff <abarinoff@gmail.com>
2016-12-29 13:53:47 +0200

    - Simple try-outs based on Create-react-app npm module

---
+++ src/Site.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Page from './Page';
+
+class Site extends Component {
+    render() {
+        return (
+            <div>
+                <h1>KoawareSite</h1>
+                <Page/>
+            </div>
+        );
+    }
+}
+
+export default Site;
