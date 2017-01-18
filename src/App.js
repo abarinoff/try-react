fe175dd41eb7832aa39d1f2b6606e3d97c0dff26
abarinoff <abarinoff@gmail.com>
2017-01-18 17:32:48 +0200

    - Started writing TODO application based on example provided on Redux site

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './App.css';
 
 class App extends Component {
 
-
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Application</h2>
+        <div className="container">
+            <ul className="list-group">
+                <li className="list-group-item">Cras justo odio</li>
+                <li className="list-group-item">Dapibus ac facilisis in</li>
+                <li className="list-group-item">Morbi leo risus</li>
+                <li className="list-group-item">Porta ac consectetur ac</li>
+                <li className="list-group-item">Vestibulum at eros</li>
+            </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Site from './Site';
+import {combineReducers, createStore} from 'redux';
+
+import App from './App';
+import {ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER} from './actions';
+import {VisibilityFilters} from './actions';
+
 import './index.css';
 
+const initialState = {
+    visibilityFilter: VisibilityFilters.SHOW_ALL,
+    todos: []
+};
+
+function visibilityFilter(state = VisibilityFilters.SHOW_ALL, action) {
+    console.log("visibilityFilter reducer was called");
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.payload.filter;
+        default:
+            return state;
+    }
+}
+
+function todos(state = [], action) {
+    console.log("todos reducer was called");
+    switch(action.type) {
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.payload.text,
+                        completed: false
+                    }
+                ]
+            });
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if (index === action.payload.index) {
+                        return Object.assign({}, todo, {
+                            completed: !todo.completed
+                        })
+                    }
+                    return todo;
+                })
+            });
+        default:
+            return state;
+    }
+}
+
+function todoApp(state = {}, action) {
+    console.log("todoApp reducer was called");
+    return {
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+        todos: todos(state.todos, action)
+    }
+}
+
+let reducer = combineReducers({visibilityFilter, todos});
+let store = createStore(reducer);
+
+store.subscribe(() =>
+    console.log(store.getState())
+);
+
 ReactDOM.render(
-    <Site/>,
+    <App/>,
     document.getElementById('root')
 );
